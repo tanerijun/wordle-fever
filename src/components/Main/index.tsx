51b936e017e39cdbc2ddc2ce4710c4d99505c9c2
TaneriX07 <tanerivince@gmail.com>
2022-06-21 00:15:21 +0800

    Bug fixes

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
-import useDidMountEffect from '../../hooks/useDidMountEffect';
 import { getSolution } from '../../lib/getSolution';
 import Wordle from '../Wordle';
 import WinnerScreen from '../WinnerScreen';
@@ -10,23 +9,13 @@ const Main = () => {
   const [solution, setSolution] = useState({ word: '', quantity: 0 });
   const [streak, setStreak] = useState(0);
   const [round, setRound] = useState(0);
-  console.log(
-    `Main states are initialized. solution=${solution} streak=${streak} round=${round}`
-  );
-  console.log(solution);
 
   useEffect(() => {
-    console.log(
-      `Running the first useEffect, run setStreak(${getSolution().streak})`
-    );
     setStreak(getSolution().streak);
   }, []);
 
-  useDidMountEffect(() => {
+  useEffect(() => {
     const solution = getSolution();
-    console.log(
-      `Running useEffect with 'round' dependency. solution=${getSolution()}, setSolution(${solution})`
-    );
     setSolution({ word: solution.word, quantity: solution.quantity });
   }, [round]);
 
--- src/index.tsx
+++ src/index.tsx
@@ -16,10 +16,12 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <ColorModeScript />
     <App />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
